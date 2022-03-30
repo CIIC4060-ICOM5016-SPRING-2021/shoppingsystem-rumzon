edcfcba176a3a28e9b478576c46c4d7db2db8823
Omar <omar.laureano2@upr.edu>
2022-03-29 21:08:19 -0400

    Updated routes, added clear cart route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from controller import ListController
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import psycopg2
 
@@ -22,41 +22,51 @@ connection.commit()
 def empty():
     return 'Hello world'
 
-@app.route('/allusers')
+@app.route('/rumzon/Users/all')
 def all_users():
     cursor.execute('SELECT * FROM users')
     res = cursor.fetchall()
     return jsonify(res)
 
-@app.route('/allitems')
+@app.route('/rumzon/Items/all')
 def all_items():
     cursor.execute('SELECT * FROM items')
     res = cursor.fetchall()
     return jsonify(res)
 
-@app.route('/allorders')
+
+@app.route('/rumzon/Orders/all')
 def all_orders():
     cursor.execute('SELECT *, orderTotal(o_id) AS o_total FROM orders')
     res = cursor.fetchall()
     return jsonify(res)
 
-@app.route('/alllikes')
+@app.route('/rumzon/Likes/all')
 def all_likes():
     cursor.execute('SELECT * FROM likes')
     res = cursor.fetchall()
     return jsonify(res)
 
-@app.route('/allitemsinreciept')
+@app.route('/rumzon/ItemsInOrder/all')
 def all_items_in_order():
     cursor.execute('SELECT *, itemTotal(item_id, o_ammount) AS i_total FROM itemsinorder')
     res = cursor.fetchall()
     return jsonify(res)
 
-@app.route('/allitemsincart')
+@app.route('/rumzon/ItemsInCart/all')
 def all_items_in_cart():
     cursor.execute('SELECT *, itemTotal(item_id, c_ammount) AS i_total FROM itemsincart')
     res = cursor.fetchall()
     return jsonify(res)
+
+@app.route('/rumzon/ItemsInCart/clear/<string:u_id>',methods = ['DELETE','GET'])
+def clear_cart_for_user(u_id):
+    if request.method == 'DELETE':
+        cursor.execute('delete from itemsincart where u_id = ' + u_id)
+        connection.commit()
+        return "deleted"
+    else:
+        return 'el hehe'
 
 if __name__ == '__main__':
     app.run(debug=1)
