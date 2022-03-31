6e973b7dad3ffd57a073d5b48e98f52ac4b964e9
Omar <omar.laureano2@upr.edu>
2022-03-30 20:54:59 -0400

    Added Controllers, DAOs for relation tables. Added clear cart function

--- controller/ItemController.py
+++ controller/ItemController.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+import json
 from dao.ItemDAO import ItemDAO
 
 class ItemController:
@@ -31,3 +32,40 @@ class ItemController:
             return jsonify(self.dictionary(daoRes[0]))
         else:
             return jsonify('ID Not Found'), 405
+
+    def addNewItem(self, json):
+        i_name = json['i_name']
+        i_category = json['i_category']
+        i_stock = json['i_stock']
+        i_price = json['i_price']
+
+        item_id = self.dao.addNewItem(i_name, i_category, i_stock, i_price)
+        json['item_id'] = item_id
+        return jsonify(json), 201
+
+    def deleteItem(self, id):
+        self.dao.deleteItemByID(id)
+        return jsonify("Item #%s deleted" % id)
+
+    def updateItem(self, id, reqjson):
+
+        # print(self.getByID(id).json()['i_name'])
+        # i_name = oldjson['i_name']
+        # i_category = oldjson['i_category']
+        # i_stock = oldjson['i_stock']
+        # i_price = oldjson['i_price']
+
+    # if json['i_name']:
+        i_name = reqjson['i_name']
+    # if json['i_category']:
+        i_category = reqjson['i_category']
+    # if json['i_stock']:
+        i_stock = reqjson['i_stock']
+    # if json['i_price']:
+        i_price = reqjson['i_price']
+
+        daoRes = self.dao.updateItemByID(id, i_name, i_category, i_stock, i_price)
+        if daoRes:
+            return jsonify(self.dictionary(daoRes[0]))
+        else:
+            return jsonify('Error editing item with ID %d' %id), 405
